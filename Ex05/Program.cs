ab25957a829fac5d22771c4d87f37dc227a4100a
Staicu Adrian <adrianstacu888@gmail.com>
2020-02-06 21:35:22 +0200

    Add project files.

---
+++ Ex05/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 1;
+            for (int n = 2; n < 13; n++)
+            {
+                suma *= n;
+            }
+            Console.WriteLine("Factorialul este: " + suma);
+            Console.ReadLine();
+        }
+    }
+}
